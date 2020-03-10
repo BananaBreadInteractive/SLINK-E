e424057633a143f418aa87f0a524f1c63d67ca45
JacquesJnr <jacquesvisserjnr@gmail.com>
2020-03-10 17:29:44 +0000

    Added Grabcheck to arm script

--- Assets/Scripts/Arms.cs
+++ Assets/Scripts/Arms.cs
@@ -11,6 +11,11 @@ public class Arms : MonoBehaviour
     private float startTime;
     private float speed = 8f;
     private Rigidbody2D rb;
+    public Rigidbody2D player;
+
+    private float radius = 0.3f;
+    public LayerMask WhatIsGrab;
+    public bool canGrab;
 
     private void Awake()
     {
@@ -24,12 +29,16 @@ public class Arms : MonoBehaviour
         startTime = Time.time;
     }
 
-
     void OnEnable()
     {
         _controls.Player.Enable();
         _controls.Player.Arms.performed += ctx => leftArm = ctx.ReadValue<Vector2>();
         _controls.Player.Arms.canceled += ctx => leftArm = Vector2.zero;
+
+        _controls.Player.LeftHand.performed += ctx => Attatch();
+        _controls.Player.RightHand.performed += ctx => Attatch();
+        _controls.Player.LeftHand.canceled += ctx => Detatch();
+        _controls.Player.RightHand.canceled += ctx => Detatch();
     }
 
     void OnDisable()
@@ -42,5 +51,31 @@ public class Arms : MonoBehaviour
         Vector2 leftVector = new Vector2(leftArm.x, leftArm.y) * Time.deltaTime * speed;
         rb.MovePosition((Vector2)transform.position + (leftVector));
         Vector3.Lerp(armPos, transform.position, 1f);
+
+        canGrab = Physics2D.OverlapCircle(transform.position, radius, WhatIsGrab);
+
+
+    }
+
+    void Attatch()
+    {
+        if (canGrab)
+        {
+            rb.bodyType = RigidbodyType2D.Static;
+            player.bodyType = RigidbodyType2D.Dynamic;
+        }
+
+        
+    }
+
+    void Detatch()
+    {
+        rb.bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
