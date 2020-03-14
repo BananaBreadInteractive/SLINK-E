5b2bdba2593c1352ca2e10492f22889d9830a816
BananaBreadInteractive <bananabreadinteractive@gmail.com>
2020-03-14 16:45:01 +0000

    Added New Springs to player

--- Assets/Scripts/Arms.cs
+++ Assets/Scripts/Arms.cs
@@ -7,22 +7,26 @@ public class Arms : MonoBehaviour
     public Transform leftHand, rightHand;
     public Transform leftOrigin, rightOrigin;
     public LineRenderer leftSpring, rightSpring; //The arms of the player
+    private Mesh leftMesh;
+    private bool useTransform;
 
-  
 
     void Start()
     {
-
+        
     }
 
     // Sets the position of the line renderer from the shoulders of the player to the start of the arms
     private void Update()
-    {
+    { 
+
         leftSpring.SetPosition(0, leftOrigin.localPosition);
         leftSpring.SetPosition(1, leftHand.localPosition);
 
         rightSpring.SetPosition(0, rightOrigin.localPosition);
         rightSpring.SetPosition(1, rightHand.localPosition);
+
+        leftSpring.BakeMesh(leftMesh, useTransform = false);
     }
 
 }
