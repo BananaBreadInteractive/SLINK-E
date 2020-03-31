f83793e2ba131c2b87a61156fe73531a9b4c1f9f
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-03-31 18:00:26 +0100

    Added "Demo" Scene
    This scene will be used for testing, thus far I've started adding menu elements

---
+++ Assets/Scripts/MenuLogo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLogo : MonoBehaviour
+{
+    public LineRenderer spring;
+    public GameObject top, bottom;
+
+    private void Update()
+    {
+        spring.SetPosition(0, top.transform.localPosition);
+        spring.SetPosition(1, bottom.transform.localPosition);
+    }
+}
