2060f4ddfed38d3364987986a582045b2acd05ee
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-05-07 19:39:46 +0100

    Added Intro Cutscene and tweaked player physics

--- Assets/Scripts/Grab.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
-
-public class Grab : MonoBehaviour //  Changed the sprites of the players hands when the grab buttons are pressed
-{
-   
-}
